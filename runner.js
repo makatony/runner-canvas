d1e4fb367200369111a4eb10598b7c71cf968cc7
Tony Kumin <tony@kumin.ch>
2018-02-03 21:04:53 +0100

    basic game with walldistance as input to neural network

---
+++ runner.js
@@ -0,0 +1,64 @@
+class Runner {
+
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.heading = 0;
+
+    this.wallDistance = [99, 99, 99, 99]; // to be used as input for neural network
+    this.sightDist = 20; //assumes everything beyond 19 pixels is a wall
+  }
+
+  update() {
+    this.pos.x++;
+    this.calcWallDistance();
+    if (this.wallDistance[0] == 0) console.log('"dead"');
+  }
+
+  draw() {
+    stroke(255, 0, 0);
+    strokeWeight(2);
+    let thisHeading = this.heading;
+    for (let h = 0; h < 4; h++) {
+      thisHeading = this.heading + h / 2 * Math.PI;
+      let vector = radian2cartesian(this.pos, thisHeading, this.wallDistance[h]);
+      line(this.pos.x, this.pos.y, vector.x, vector.y);
+    }
+    
+    fill(0);
+    noStroke();
+    ellipse(this.pos.x, this.pos.y, 5);
+  }
+
+  calcWallDistance() {
+    let pixelCol = [];
+    let thisHeading, vector, pxlIndex;
+
+    for (let h = 0; h < 4; h++) {
+      thisHeading = this.heading + h / 2 * Math.PI;
+      // for each heading direction, check up to this.sightDistance pixels
+      for (let d = 0; d < this.sightDist; d++) { //calculating distance to wall based on 4 sights
+        this.wallDistance[h] = d;
+        vector = radian2cartesian(this.pos, thisHeading, d);
+        //ignore cases at the edge of the screen
+        if (vector.x < 0 || vector.x > width || vector.y < 0 || vector.y > height) break;
+        //get the pixel color to compare
+        pxlIndex = Math.floor((vector.x + width * vector.y) * pixelDensity() * 4);
+        pixelCol = color([pixels[pxlIndex + 0], pixels[pxlIndex + 1], pixels[pxlIndex + 2]]);
+        //stop adding to walldistance as soon as a wall is seen
+        if (pixelCol.toString() == BGCOLOR.toString()) break;
+      }
+    }
+  }
+
+
+  static update() {
+    for (let i = 0; i < runners.length; i++) {
+      runners[i].update();
+    }
+  }
+  static draw() {
+    for (let i = 0; i < runners.length; i++) {
+      runners[i].draw();
+    }
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,126 @@
+let paths = [];
+currPath = 0;
+paths[currPath] = [];
+runners = [];
+const PATHCOLOR = [255, 255, 255, 255];
+
+
+function setup() {
+  createCanvas(640, 480);
+  BGCOLOR = color(120);
+  fr = createP("framerate");
+
+  mouseListeners.push({
+    type: 'mouseDragged',
+    fn: function () {
+      if (this.isDoubleClick) {}
+      let thisPath = paths[currPath];
+      if (thisPath.length > 1 && dist(mouseX, mouseY, thisPath[thisPath.length - 1].x, thisPath[thisPath.length - 1].y) < 10)
+        return;
+      thisPath.push(createVector(mouseX, mouseY));
+    }
+  });
+
+  mouseListeners.push({
+    type: 'mouseReleased',
+    fn: function () {
+      if (this.isDoubleClick) {}
+      currPath++;
+      paths[currPath] = [];
+    }
+  });
+
+  mouseListeners.push({
+    type: 'mousePressed',
+    fn: function () {
+      if (this.isDoubleClick) {
+        runners.push(new Runner(mouseX, mouseY));
+      }
+    }
+  });
+}
+
+function draw() {
+  background(BGCOLOR);
+
+  for (let p = 0; p < paths.length; p++) {
+    let thisPath = paths[p];
+    for (let i = 1; i < thisPath.length; i++) {
+      stroke(PATHCOLOR);
+      strokeWeight(20);
+      line(thisPath[i].x, thisPath[i].y, thisPath[i - 1].x, thisPath[i - 1].y);
+    }
+  }
+
+  //getting pixels once in a while after drawing the path
+  if (Math.floor(frameRate()) % 30 == 0) loadPixels();
+
+  Runner.update();
+  Runner.draw();
+
+
+  fr.html(frameRate());
+  // for (let p = 0; p < paths.length; p++) {
+  //   let thisPath = paths[p];
+  //   for (let i = 1; i < thisPath.length; i++) {
+  //     fill(0);
+  //     noStroke();
+  //     ellipse(thisPath[i].x, thisPath[i].y, 5);
+  //   }
+  // }
+
+
+}
+
+
+
+
+
+// ############################
+// #### UTIL: MOUSE EVENTS ####
+// ############################
+
+let doubleClickMS = 0;
+let isDoubleClick = false;
+let isMouseDrag = false;
+let mouseListeners = [];
+
+var mousePressed = function () {
+  this.isDoubleClick = (floor(millis() - doubleClickMS) <= 500 ? true : false); //for some reason this.isDoubleClick is passed to the functions without problems
+  doubleClickMS = millis(); //resets doubleclick timer
+
+  mouseEventCallHandlers('mousePressed', arguments);
+  this.isMouseDrag = false;
+};
+var mouseClicked = function () {
+  mouseEventCallHandlers('mouseClicked', arguments);
+  this.isMouseDrag = false;
+};
+var mouseReleased = function () {
+  mouseEventCallHandlers('mouseReleased', arguments);
+  this.isMouseDrag = false;
+};
+var mouseDragged = function () { // mouseDragged is a P5 function. we use it to call anything that was registered as a mouseDragged event
+  this.isMouseDrag = true;
+  mouseEventCallHandlers('mouseDragged', arguments);
+};
+let mouseEventCallHandlers = function (type, arguments) {
+  mouseListeners.forEach(function (elt) {
+    if (elt.type == type) elt.fn.apply(this, arguments);
+  });
+};
+
+
+function cartesian2radian(v1, v2) {
+  var deltaX = v2.x - v1.x;
+  var deltaY = v2.y - v1.y;
+  var rad = Math.atan2(deltaY, deltaX);
+  // var deg = rad * (180 / Math.PI);
+  return rad;
+}
+
+function radian2cartesian(point, angle, length) {
+  let x2 = point.x + Math.cos(angle) * length;
+  let y2 = point.y + Math.sin(angle) * length;
+  return createVector(x2, y2);
+}
